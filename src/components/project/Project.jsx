c0b19ceb0c09a3b4db53d1b1d40413af9022d0a7
Rahul Chauhan <rahulchauhan962103@gmail.com>
2025-05-01 17:10:55 +0530

    adding project section

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Title from '../Layouts/Title'
+import ProjectCard from './ProjectCard'
+import {projectOne, projectTwo, projectThree} from '../../assets/index'
 
 const Project = () => {
   return (
@@ -10,6 +12,40 @@ const Project = () => {
             des="My Project"
             />
         </div>
+
+        <div className='grid grid-cols-3 gap-14'>
+            <ProjectCard
+              title="SOCIAL MEDIA CLONE"
+              des="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+              src={projectOne}
+            />
+            <ProjectCard
+              title="SOCIAL MEDIA CLONE"
+              des="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+              src={projectTwo}
+            />
+            <ProjectCard
+              title="SOCIAL MEDIA CLONE"
+              des="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+              src={projectThree}
+            />
+            <ProjectCard
+              title="SOCIAL MEDIA CLONE"
+              des="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+              src={projectOne}
+            />
+            <ProjectCard
+              title="SOCIAL MEDIA CLONE"
+              des="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+              src={projectOne}
+            />
+            <ProjectCard
+              title="SOCIAL MEDIA CLONE"
+              des="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+              src={projectOne}
+            />
+        </div>
+
     </section>
   )
 }
---
+++ src/components/project/ProjectCard.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { BsGithub } from 'react-icons/bs'
+import { FaGlobe } from 'react-icons/fa'
+
+const ProjectCard = ({title,des,src}) => {
+  return (
+    <div className='w-full px-12 h-auto py-10 rounded-lg flex flex-col bg-gradient-to-r from-[#212428] to-[#202327] group hover:bg-gradient-to-b hover:from-gray-900 hover:gray-900 transition-colors duration-1000  [box-shadow:7px_9px_18px_#1c1022,_-7px_-9px_19px_#262a2e]'>
+        <div className='w-full h-[80%] overflow-hidden rounded-lg'>
+            <img 
+              src={src} 
+              alt=""
+              className='group-hover:scale-110 duration-300 cursor-p' 
+
+            />
+        </div>
+        <div className='w-full mt-5 flex flex-col gap-6'>
+            <div className='flex items-center justify-between'>
+              <h3 className='text-base uppercase text-[#ff014f]'>{title}</h3>
+              <div className='flex gap-2'>
+                <span className='text-lg w-10 h-10 rounded-full bg-black inline-flex justify-center items-center text-gray-400 hover:text-[#ff014f] duration-300 cursor-pointer'>
+                    <BsGithub/>
+                </span>
+                <span className='text-lg w-10 h-10 rounded-full bg-black inline-flex justify-center items-center text-gray-400 hover:text-[#ff014f] duration-300 cursor-pointer'>
+                    <FaGlobe/>
+                </span>
+              </div>
+            </div>
+            <div>
+                <p className='text-base tracking-wide mt-3 hover:text-gray-100 duration-300'>{des}</p>
+            </div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default ProjectCard
